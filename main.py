c7ba2e03371efbecfda42a48c671d9f1dd68018b
Julien Hermine-Bromberg <julien.hermine-bromberg@epitech.eu>
2018-10-10 15:32:22 +0200

    second course ended, basics of routing have been seen

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return 'This is the homepage!'
 
+@app.route('/test')
+def test():
+    return '<h2>It\'s a test !</h2>'
+
+@app.route('/profile/<username>')
+def profile(username):
+    return '<h1>Welcome %s</h1>' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return '<h1>Post ID is %s</h1>' % post_id
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    apt.run(debug = True)
